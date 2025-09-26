71a92617e3b1f5319f48f65854fd9898d51c17fd
danymorar20 <danymorar20@gmail.com>
2025-09-26 14:13:39 -0600

    updating and deleting files from src

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MySQLConfig } from './config/database/database.config';
+import { DatabaseModule } from './config/database/database.module';
+import { CustomerModule } from './features/customers/customer.module';
+import { AddressModule } from './features/addresses/address.module';
+import { OrderModule } from './features/orders/order.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+      envFilePath: '.env',
+      load: [MySQLConfig],
+    }),
+    OrderModule,
+    AddressModule,
+    DatabaseModule,
+    CustomerModule,
+  ],
 })
 export class AppModule {}
